b3f40cffa1abad48d4cd0e14d7e2e9519086a5c5
aemelius <simoneperfect@gmail.com>
2016-08-11 21:40:26 +0100

    green

--- src/cloogling/core.clj
+++ src/cloogling/core.clj
@@ -1,11 +1,11 @@
 (ns cloogling.core
   (:require [clojure.data.json :as json]))
 
-(defrecord entry [url])
+(defrecord entry [url title])
 
 (defn get-entries
   "Get results from json data retrieved from engine"
   [x]
-  (map (fn [x] (->entry (x "Url" ))) (-> x
+  (map (fn [x] (->entry (x "Url" ) (x "Title"))) (-> x
                                          json/read-str
                                          (get-in ["d" "results"]))))
