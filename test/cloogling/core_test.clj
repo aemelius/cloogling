3f5c5f5c73e4ef76d4892f605e8f0f93b801bafb
aemelius <simoneperfect@gmail.com>
2016-08-11 20:47:02 +0100

    Test

--- test/cloogling/core_test.clj
+++ test/cloogling/core_test.clj
@@ -3,3 +3,8 @@
             [expectations :refer :all]))
 
 (expect 1 1)
+
+(def miles_davis_data (slurp "test/cloogling/miles_davis_bing.json"))
+
+;; Parsing entries in bing json format to a list
+(expect 10 (count (get-entries miles_davis_data)))
