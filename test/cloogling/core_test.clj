b32911153c06480cddfbc43a9075c0ba35ebf03f
aemelius <simoneperfect@gmail.com>
2016-08-11 22:29:41 +0100

    Added a few comments, plus auto-indenting

--- test/cloogling/core_test.clj
+++ test/cloogling/core_test.clj
@@ -2,14 +2,13 @@
   (:require [cloogling.core :refer :all]
             [expectations :refer :all]))
 
-(expect 1 1)
+;; starting with parsing Bing-style results, with non-edge cases
 
+;; setup: load a standard json response from Bing, with 10 entries
 (def miles_davis_data (slurp "test/cloogling/miles_davis_bing.json"))
-
 
 ;; Counting entries in standard test data
 (expect 10 (count (get-entries miles_davis_data)))
-
 
 ;; test on the first entry's url
 (expect "https://en.wikipedia.org/wiki/Miles_Davis"
@@ -35,7 +34,10 @@
             first
             (:snippet))
         )
-;; if the data is not a valid json, I expect an empty list of entries
+
+;; now some slightly edgier cases
+
+;; if the data is not a valid json, I expect an empty list to be returned
 (expect [] (get-entries "this is not a valid bing like json"))
 
 ;; if the data is valid json, but is in a different structure than what I expect from Bing, I will return an empty list
