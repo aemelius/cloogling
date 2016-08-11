46711a82c2e50e1c8c95268dc281a50afb902c88
aemelius <simoneperfect@gmail.com>
2016-08-11 21:07:39 +0100

    green

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [expectations "1.4.52"]]
+                 [expectations "1.4.52"]
+                 [org.clojure/data.json "0.2.6"]]
   :plugins [[lein-autoexpect "1.0"]])
--- src/cloogling/core.clj
+++ src/cloogling/core.clj
@@ -1,6 +1,9 @@
-(ns cloogling.core)
+(ns cloogling.core
+  (:require [clojure.data.json :as json]))
 
 (defn get-entries
   "Get results from json data retrieved from engine"
   [x]
-  x)
+  (-> x
+      json/read-str
+      (get-in ["d" "results"])))
