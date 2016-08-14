640b905fb306247545ca896a6635b9c8117b865d
aemelius <simoneperfect@gmail.com>
2016-08-14 22:54:21 +0100

    Polishing project metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cloogling "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Cloogling: Clojure + google + bing"
+  :url "https://github.com/aemelius/cloogling"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
